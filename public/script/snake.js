4ca6392322b9a6ee119e76e52f4c97ee6e451d2c
Hong Yue Wang <hongyuew26@gmail.com>
2020-05-11 20:47:18 -0400

    Game is now playable

--- public/script/snake.js
+++ public/script/snake.js
@@ -18,13 +18,18 @@ function checkSupported() {
         this.gridSize = 10;
 
         // Moves snake every 100 milliseconds
-        setInterval(moveSnake,100);
+        interval = setInterval(moveSnake,100);
 
         snakeLength = 3;
         makeFoodItem();
         drawSnake();
 
+        allowPressKeys = true;
+
         document.onkeydown = function(event) {
+            if (!allowPressKeys) {
+                return null;
+            }
             var keyCode;
 
             if (event === null) {
@@ -36,21 +41,25 @@ function checkSupported() {
             switch(keyCode) {
                 // left
                 case 37:
+                    if (direction == 'right') return;
                     moveLeft();
                     break;
 
                 // up
                 case 38:
+                    if (direction == 'down') return;
                     moveUp();
                     break;
 
                 // right
                 case 39:
+                    if (direction == 'left') return;
                     moveRight();
                     break;
 
                 // down
                 case 40:
+                    if (direction == 'up') return;
                     moveDown();
                     break;
 
@@ -86,23 +95,6 @@ function checkSupported() {
         alert("Unfortunately, your browser does not support the canvas"
         + " tag. Please use any web browser other than Internet Explorer."
         + " Seriously. Why are you using Internet Explorer?");
-    }
-}
-
-function drawSnake() {
-    snakeBody.push([currentPosition['x'], currentPosition['y']]);
-    ctx.fillStyle = "#32CD32";
-    ctx.fillRect(currentPosition['x'], currentPosition['y'], gridSize, gridSize);
-    if (snakeBody.length > snakeLength) {
-        var itemToRemove = snakeBody.shift();
-        ctx.clearRect(itemToRemove[0], itemToRemove[1], gridSize, gridSize);
-    }
-
-    if (currentPosition['x'] == suggestedPoint[0] && currentPosition['y'] ==
-    suggestedPoint[1]) {
-        makeFoodItem();
-        snakeLength += 1;
-        //updatescore
     }
 }
 
@@ -147,7 +139,7 @@ function moveLeft() {
     if (leftPosition() >= 0) {
         executeMove('left', 'x', leftPosition());
     } else {
-        whichWayToGo('y');
+        gameOver();
     }
 }
 
@@ -155,7 +147,7 @@ function moveRight() {
     if (rightPosition() < canvas.width) {
         executeMove('right', 'x', rightPosition());
     } else {
-        whichWayToGo('y');
+        gameOver();
     }
 }
 
@@ -163,7 +155,7 @@ function moveUp() {
     if (upPosition() >= 0) {
         executeMove('up', 'y', upPosition());
     } else {
-        whichWayToGo('x');
+        gameOver();
     }
 }
 
@@ -171,7 +163,7 @@ function moveDown() {
     if (downPosition() < canvas.height) {
         executeMove('down', 'y', downPosition());
     } else {
-        whichWayToGo('x');
+        gameOver();
     }
 }
 
@@ -192,11 +184,14 @@ function whichWayToGo(axisType) {
     }
 }
 
+
 function makeFoodItem() {
     // Not declared, therefore it is a global variable
     suggestedPoint = [Math.floor(Math.random() * (canvas.width/gridSize)) *
     gridSize, Math.floor(Math.random() * (canvas.height/gridSize)) * gridSize];
-    if (snakeBody.some(hasPoint)) {
+    if (snakeBody.some(hasPoint) || suggestedPoint[0] == 0 ||
+    suggestedPoint[0] == canvas.width - gridSize || suggestedPoint[1] == 0 ||
+    suggestedPoint[1] == canvas.height - gridSize) {
         makeFoodItem();
     } else {
         ctx.fillStyle = "red";
@@ -206,4 +201,39 @@ function makeFoodItem() {
 
 function hasPoint(element, index, array) {
     return (element[0] == suggestedPoint[0] && element[1] == suggestedPoint[1]);
+}
+
+function drawSnake() {
+    if (snakeBody.some(hasEatenItself)) {
+        gameOver();
+        return false;
+    }
+    snakeBody.push([currentPosition['x'], currentPosition['y']]);
+    ctx.fillStyle = "#32CD32";
+    ctx.fillRect(currentPosition['x'], currentPosition['y'], gridSize, gridSize);
+    if (snakeBody.length > snakeLength) {
+        var itemToRemove = snakeBody.shift();
+        ctx.clearRect(itemToRemove[0], itemToRemove[1], gridSize, gridSize);
+    }
+
+    if (currentPosition['x'] == suggestedPoint[0] && currentPosition['y'] ==
+    suggestedPoint[1]) {
+        makeFoodItem();
+        snakeLength += 1;
+    }
+}
+
+function hasEatenItself(element, index, array) {
+    return (element[0] == currentPosition['x'] && element[1] ==
+    currentPosition['y']);
+}
+
+function gameOver() {
+    var score = (snakeLength - 3) * 10;
+    clearInterval(interval);
+    snakeBody = [];
+    snakeLength = 3;
+    allowPressKeys = false;
+    alert("Game over. Your score was " + score);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
